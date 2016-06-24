7f7f607baee8e1015d3d1b7eb178ad2e48aa69d8
Yakup Adaklı <yakup.adakli@gmail.com>
2016-06-24 11:10:52 +0300

    Project name fix.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialAuthExample.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialauthexample.settings")
 
     from django.core.management import execute_from_command_line
 
--- socialauthexample/wsgi.py
+++ socialauthexample/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for socialAuthExample project.
+WSGI config for socialauthexample project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialAuthExample.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialauthexample.settings")
 
 application = get_wsgi_application()
